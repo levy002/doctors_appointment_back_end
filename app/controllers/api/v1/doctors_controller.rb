9d2ba70d18e0eb3f72f7a24771f98858403194f6
levy002 <levy.ukwi002@gmail.com>
2022-08-12 17:05:27 +0200

    Fix linters

--- app/controllers/api/v1/doctors_controller.rb
+++ app/controllers/api/v1/doctors_controller.rb
@@ -1,9 +1,8 @@
 class Api::V1::DoctorsController < ApplicationController
-
   def index
     doctors = Doctor.all
     if doctors
-      render json: { status: 'SUCCESS', data: doctors, message: 'All doctors loaded successfully!'}, status: :ok
+      render json: { status: 'SUCCESS', data: doctors, message: 'All doctors loaded successfully!' }, status: :ok
     else
       render json: doctor.errors, status: :bad_request
     end
@@ -12,17 +11,17 @@ class Api::V1::DoctorsController < ApplicationController
   def show
     doctor = Doctor.find(params[:id])
     if doctor
-      render json: { status: 'SUCCESS', data: doctor, message: 'Doctor loaded successfully!'}, status: :ok
+      render json: { status: 'SUCCESS', data: doctor, message: 'Doctor loaded successfully!' }, status: :ok
     else
       render json: doctor.errors, status: :bad_request
     end
   end
 
   def create
-  doctor = Doctor.new(doctor_params)
+    doctor = Doctor.new(doctor_params)
 
     if doctor.save
-      render json: { status: 'SUCCESS', data: doctor, message: 'Doctor created successfully!'}, status: :created
+      render json: { status: 'SUCCESS', data: doctor, message: 'Doctor created successfully!' }, status: :created
     else
       render json: doctor.errors, status: :unprocessable_entity
     end
@@ -31,14 +30,15 @@ class Api::V1::DoctorsController < ApplicationController
   def destroy
     doctor = Doctor.find(params[:id])
     if doctor.destroy!
-    render json: {message: 'Doctor deleted successfully'}, status: :ok
+      render json: { message: 'Doctor deleted successfully' }, status: :ok
     else
       render json: doctor.errors, status: :bad_request
     end
   end
 
   private
-    def doctor_params
-      params.require(:doctor).permit(:name, :fee, :image, :details)
-    end
+
+  def doctor_params
+    params.require(:doctor).permit(:name, :fee, :image, :details)
+  end
 end
--- spec/requests/doctor_spec.rb
+++ spec/requests/doctor_spec.rb
@@ -3,14 +3,13 @@ require 'rails_helper'
 describe 'doctors API', type: :request do
   describe 'GET /doctors' do
     before do
-      FactoryBot.create(:doctor, name: 'Doctor-1',fee: 20, image: 'Doctor-1-photo', details: 'I am Doctor-1')
+      FactoryBot.create(:doctor, name: 'Doctor-1', fee: 20, image: 'Doctor-1-photo', details: 'I am Doctor-1')
       FactoryBot.create(:doctor, name: 'Doctor-2', fee: 30, image: 'Doctor-2-photo', details: 'I am Doctor-2')
       FactoryBot.create(:doctor, name: 'Doctor-3', fee: 40, image: 'Doctor-3-photo', details: 'I am Doctor-3')
     end
     it 'returns all doctors' do
-      
       get '/api/v1/doctors'
-  
+
       expect(response).to have_http_status(:success)
       expect(JSON.parse(response.body).size).to eq(3)
     end
@@ -18,25 +17,31 @@ describe 'doctors API', type: :request do
 
   describe 'POST /doctors' do
     it 'create a new doctor' do
-      expect {
+      expect do
         post '/api/v1/doctors', params: {
-          doctor: {name: 'Doctor',fee: 20, image: 'Doctor-photo', details: 'I am Doctor'}
-          }
-        }.to change { Doctor.count}.from(0).to(1)
+          doctor: { name: 'Doctor', fee: 20, image: 'Doctor-photo', details: 'I am Doctor' }
+        }
+      end.to change { Doctor.count }.from(0).to(1)
 
       expect(response).to have_http_status(:created)
     end
   end
 
   describe 'DELETE /doctors:id' do
-    let!(:doctor1) {FactoryBot.create(:doctor, name: 'Doctor-1',fee: 20, image: 'Doctor-1-photo', details: 'I am Doctor-1')}
-    let!(:doctor2) { FactoryBot.create(:doctor, name: 'Doctor-2', fee: 30, image: 'Doctor-2-photo', details: 'I am Doctor-2')}
-    let!(:doctor3) { FactoryBot.create(:doctor, name: 'Doctor-3', fee: 40, image: 'Doctor-3-photo', details: 'I am Doctor-3')}
-    
+    let!(:doctor1) do
+      FactoryBot.create(:doctor, name: 'Doctor-1', fee: 20, image: 'Doctor-1-photo', details: 'I am Doctor-1')
+    end
+    let!(:doctor2) do
+      FactoryBot.create(:doctor, name: 'Doctor-2', fee: 30, image: 'Doctor-2-photo', details: 'I am Doctor-2')
+    end
+    let!(:doctor3) do
+      FactoryBot.create(:doctor, name: 'Doctor-3', fee: 40, image: 'Doctor-3-photo', details: 'I am Doctor-3')
+    end
+
     it 'delete a doctor' do
-      expect {
+      expect do
         delete "/api/v1/doctors/#{doctor1.id}"
-      }.to change { Doctor.count}.from(3).to(2)
+      end.to change { Doctor.count }.from(3).to(2)
 
       expect(response).to have_http_status(:ok)
     end
