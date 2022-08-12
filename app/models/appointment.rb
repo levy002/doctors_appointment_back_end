c08037599cdb25a5e1fcc44eeb2632533bd4c0a7
ridaarif98 <ridaarif16@gmail.com>
2022-08-12 13:02:51 +0200

    Add validation in models

---
+++ app/models/appointment.rb
@@ -0,0 +1,6 @@
+class Appointment < ApplicationRecord
+  belongs_to :user
+  belongs_to :doctor
+  validates :city, presence: true, length: { minimum: 3, maximum: 15} 
+  validates :appointment_time, presence: true, availability: true
+end
---
+++ app/models/user.rb
@@ -0,0 +1,4 @@
+class User < ApplicationRecord
+    has_many :appointments, dependent: :destroy
+    validates :name, presence: true, length: {minimum: 3, maximum: 15}
+end
