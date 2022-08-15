65edeccc33de17550305bfbd0f14d8404758423f
levy002 <levy.ukwi002@gmail.com>
2022-08-16 00:46:42 +0200

    Validate e-mail to be unique

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :appointments, dependent: :destroy
   validates :name, presence: true, length: { minimum: 3, maximum: 15 }
+  validates :email, uniqueness: true
 end
