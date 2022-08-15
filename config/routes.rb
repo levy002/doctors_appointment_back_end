545185d55acbd2eef4906a83fd6cd11cf014113a
direakanbi <direakanbi@icloud.com>
2022-08-15 05:20:50 -0700

    Added appropriate routing for api

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,12 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :doctors
+      resources :appointments, only: %i[index]
+      get "/appointment/:id", to: "appointments#show"
+      post "/getAppointment", to: "appointments#getAppointment"
+      post "/appointment", to: "appointments#create"
+      put "/appointment/:id", to: "appointments#update" 
+      delete "/appointment/:id", to: "appointments#destroy"
     end
   end
 end
