426565adba229ecca822902a0cb618513fc67481
direakanbi <direakanbi@icloud.com>
2022-08-15 10:28:09 -0700

    Adjusted routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-
   post 'api/login', to: 'users#login'
   post 'api/register', to: 'users#signup'
-
+  
   namespace :api do
     namespace :v1 do
       resources :doctors
-      resources :users do
-        resources :appointments, only: %i[index]
-        get '/appointment/:id', to: 'appointments#show'
-        post '/getAppointment', to: 'appointments#getappointment'
-        post '/appointment', to: 'appointments#create'
-        put '/appointment/:id', to: 'appointments#update'
-        delete '/appointment/:id', to: 'appointments#destroy'
-      end
+      resources :appointments
     end
   end
 end
