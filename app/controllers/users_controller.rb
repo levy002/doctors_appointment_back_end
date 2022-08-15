deb650531aac7dc34904f25547ab16a5e2205547
levy002 <levy.ukwi002@gmail.com>
2022-08-15 10:47:40 +0200

    Creating login and sign up logic

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,35 @@
+class UsersController < ApplicationController
+  def login
+    @user = User.find_by_email!(params[:email])
+    if @user&.valid_password?(params[:password])
+      render json: { data: @user }
+    else
+      render json: { error: 'Invalid login details' }, status: :unauthorized
+    end
+  end
+
+  def signup
+    if params[:name].present? &&
+       params[:email].present? &&
+       params[:password].present? &&
+       params[:password_confirmation].present?
+      if params[:password] == params[:password_confirmation]
+        @user = User.create(
+          name: allowed_params[:name], email: allowed_params[:email],
+          password: params[:password], password_confirmation: params[:password_confirmation]
+        )
+        render json: { success: 'Successfully signed up, you can now login', data: @user }, status: :created
+      else
+        render json: { error: 'Password missmatch' }, status: :conflict
+      end
+    else
+      render json: { error: 'missing signup details' }, status: :partial_content
+    end
+  end
+
+  private
+
+  def allowed_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
