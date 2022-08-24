cc4da472f992174c1b1c039b265cc26eac2a5852
levy002 <levy.ukwi002@gmail.com>
2022-08-24 02:40:29 +0200

    Generate API documentation with SWAGGER

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   devise_for :users
   post 'api/login', to: 'users#login'
   post 'api/register', to: 'users#signup'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,32 @@
-Doctor.create!([
-                 {
-                   name: 'Doctor-1',
-                   fee: 20,
-                   image: 'levy-photo',
-                   details: 'I am Doctor-1'
-                 },
-                 {
-                   name: 'Doctor-2',
-                   fee: 60,
-                   image: 'Doctor-2-photo',
-                   details: 'I am Doctor-2'
-                 },
-                 {
-                   name: 'Doctor-3',
-                   fee: 40,
-                   image: 'Doctor-3-photo',
-                   details: 'I am Doctor-3'
-                 }
-               ])
+Doctor.create!([                                     
+  {
+    name: "Dr Owen",                                                
+  details: "Internal Medecine",                                   
+  image:"https://cdn.pixabay.com/photo/2017/05/23/17/12/doctor-2337835_960_720.jpg",
+  fee: 100
+  },
+  {
+  name: "Dr Chang",                                               
+  details: "Dermatologist",                                       
+  image:"https://cdn.pixabay.com/photo/2020/03/14/17/05/virus-4931227__340.jpg",
+  fee: 50
+  },
+  {
+    name: "Dr Levy",
+    details: "Psychologist",
+    image:"https://avatars.githubusercontent.com/u/87197412?s=400&u=08134e683e4128eba4edf0cba460849eccbb6eba&v=4",
+    fee: 50
+  },
+  {
+    name: "Dr Rida",
+    details: "Dentist",
+    image: "https://avatars.githubusercontent.com/u/72971752?v=4",
+    fee: 200
+  },
+  {
+    name: "Dr Akanbi",
+  details: "Surgeon",
+  image: "https://avatars.githubusercontent.com/u/83509786?v=4",
+  fee: 300
+  }
+  ]);
